f780dfcf12614b0e5f7ec421b06ab20e69d64b97
aadithyancodes <linuxtech9696@gmail.com>
2026-05-03 22:42:25 +0530

    update tasks

---
+++ python-basics/task_01_rectangle_area/main.py
@@ -0,0 +1,9 @@
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length*width
+
+if area>100:
+    print("That is a large room.")
+else:
+    print("Small room.")
