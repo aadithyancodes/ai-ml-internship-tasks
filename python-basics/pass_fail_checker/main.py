ed04ecd626749317c18c32f691b06778fa87cc10
Aadithyan S <linuxtech9696@gmail.com>
2026-05-12 22:45:45 +0530

    Add pass/fail checker task

---
+++ python-basics/pass_fail_checker/main.py
@@ -0,0 +1,8 @@
+sub1 = int(input("enter the mark of chemistry: "))
+sub2 = int(input("enter the mark of physics: "))
+sub3 = int(input("enter the mark of maths: "))
+total = sub1+sub2+sub3
+if total>=40:
+    print("PASS")
+else:
+    print("FAIL")   
