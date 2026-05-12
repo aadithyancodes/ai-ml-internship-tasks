934fcd056599be6a39c5bf95a1bf887f601810ed
Aadithyan S <73014160+aadithyancodes@users.noreply.github.com>
2026-05-12 22:23:26 +0530

    Delete python-basics/task_01_rectangle_area directory

--- python-basics/task_01_rectangle_area/main.py
+++
@@ -1,9 +0,0 @@
-length = float(input("Enter length: "))
-width = float(input("Enter width: "))
-
-area = length*width
-
-if area>100:
-    print("That is a large room.")
-else:
-    print("Small room.")
